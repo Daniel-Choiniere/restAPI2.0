10934913e237968ad4c4131ea04eeb6b077e379c
Daniel Choiniere <dpchoiniere@gmail.com>
2019-02-12 19:28:13 -0500

    handled a GET request to our API

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 // setup express app
 const app = express();
 
+//a client making a making a GET request --  We want to return some data to the client
+app.get('/', function(req, res){
+    console.log('Get request');
+    res.send({name:'Yoshi'});
+})
+
 // set up to listen for requests
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(process.env.PORT || 4000, function() {
     console.log('Now listening for requests');
 });
